e713364873108a9db98ba1546ebb2e09fd3649de
Kenneth Moreland <morelandkd@ornl.gov>
2024-07-10 18:03:21 +0000

    Correct launch script
    Also consolidated the module loading so that it can be unified
    inside and outside of the scripts.

--- build-vtkm.bash
+++ build-vtkm.bash
@@ -1,12 +1,8 @@
-#!/bin/bash                                                                                                                                                                                                      
-module reset
-module use /soft/preview-modulefiles/24.086.0
-module load oneapi/eng-compiler/2024.04.15.002
-module load intel_compute_runtime/release/821.36 
-module use /soft/modulefiles/
-module load cmake
-module load git-lfs
-module load ninja
+#!/bin/bash
+
+fullscriptpath=`realpath $0`
+scriptdir=`dirname "$fullscriptpath"`
+. $scriptdir/setup-modules.bash
 
 # from Renzo on slack 02/01/2024
 export IGC_FunctionCloningThreshold=1
--- run-vtkm-tests.bash
+++ run-vtkm-tests.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+fullscriptpath=`realpath $0`
+scriptdir=`dirname "$fullscriptpath"`
+. $scriptdir/setup-modules.bash
+
 # Find VTK-m build directory
-vtkm_build_dir=`pwd`/build/vtkm
+vtkm_build_dir=`realpath build/vtkm`
 if [ \! -d $vtkm_build_dir ] ; then
 	echo "Cannot find VTK-m build dir at $vtkm_build_dir"
+	echo "Make sure you run this script in the same directory as build-vtkm.bash"
 	exit 1
 fi
 if [ \! -f $vtkm_build_dir/bin/UnitTests_vtkm_cont_testing ] ; then
@@ -12,7 +17,7 @@ if [ \! -f $vtkm_build_dir/bin/UnitTests_vtkm_cont_testing ] ; then
 fi
 
 # Set up a launch script
-launch_dir=`pwd`/launch
+launch_dir=`realpath launch`
 mkdir -p $launch_dir
 launch_script=$(mktemp -p $launch_dir --suffix=-vtkmtest.pbs)
 echo "Launch script: $launch_script"
@@ -21,11 +26,16 @@ cat > $launch_script <<EOF
 #!/bin/bash
 #PBS -l select=1
 #PBS -l walltime=00:15:00
-#PBS -A CSC250STDA05
+#PBS -A CSC250STDA05_CNDA
 #PBS -q workq
 #PBS -N VTK-mTests
 #PBS -o $launch_dir/vtkmtest.log
 #PBS -j oe
+
+# Load modules. Note that this script may be launched non-interactively,
+# so we might need to include /etc/profile to get the module command.
+. /etc/profile
+. $scriptdir/setup-modules.bash
 
 cd $vtkm_build_dir
 ctest .
