6d13b369188a736864a605a0da329e7768f2161b
Kenneth Moreland <morelandkd@ornl.gov>
2025-05-15 15:27:42 +0000

    Update modules to latest aurora system

--- setup-modules.bash
+++ setup-modules.bash
@@ -1,9 +1,7 @@
 module reset
 module use /soft/preview-modulefiles/24.086.0
-module load oneapi/eng-compiler/2024.04.15.002
-module load intel_compute_runtime/release/821.36
+module load oneapi/release/2025.0.5
 module use /soft/modulefiles/
 module load cmake
-module load git-lfs
 module load ninja
 
